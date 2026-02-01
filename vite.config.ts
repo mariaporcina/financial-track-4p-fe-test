ca56d1e5023707c6a2258c2fd5e7ac1a2bdb0571
Maria Porcina <maria.porcina.mp@gmail.com>
2026-01-31 22:12:13 -0300

    fix: updating gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
   plugins: [
-    // tanstackRouter({
-    //   target: 'react',
-    //   autoCodeSplitting: true,
-    //   // routesDirectory: 'src/router/routes',
-    // }),
     react(),
     tailwindcss(),
   ],
