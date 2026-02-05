456e408bd8a76d54439fdf561ffcf9deb6726485
Maria Porcina <maria.porcina.mp@gmail.com>
2026-02-05 13:55:54 -0300

    fix: removing comments

--- src/queries/hooks/useGetOneTransaction.tsx
+++ src/queries/hooks/useGetOneTransaction.tsx
@@ -1,21 +1,12 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import TransactionViewModel from "../../viewModels/TransactionViewModel";
 
 export function useGetOneTransaction(id: string) {
-  // const queryClient = useQueryClient();
   const { getById } = TransactionViewModel();
 
   return useQuery({
     queryKey: ["transactions", id],
     queryFn: () => getById(id),
     enabled: !!id,
-    // initialData: () => {
-    //   const transactions =
-    //     queryClient.getQueryData<any[]>(["transactions"]);
-
-    //   return transactions?.find(t => t.id === id);
-    // },
-    // retry: true,
-    // retryDelay: 500,
   });
 }
